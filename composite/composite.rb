3dde6079aecc09b4ec84fdfc9e5b7823fde9f16d
志賀 昭駿 <a-shiga@m3career.com>
2023-08-28 13:31:46 +0900

    composite

--- composite/composite.rb
+++ composite/composite.rb
@@ -48,32 +48,32 @@ class MixTask < Task
 end
 
 # 中間のComposite
-class MakeBatterTask < Task
+# class MakeBatterTask < Task
 
-  def initialize
-    super('Make batter')
-    @sub_tasks = []
-    add_sub_task(AddDryIngredientsTask.new)
-    add_sub_task(AddLiquidsTask.new)
-    add_sub_task(MixTask.new)
-  end
+#   def initialize
+#     super('Make batter')
+#     @sub_tasks = []
+#     add_sub_task(AddDryIngredientsTask.new)
+#     add_sub_task(AddLiquidsTask.new)
+#     add_sub_task(MixTask.new)
+#   end
 
-  def add_sub_task(task)
-    @sub_tasks << task
-  end
+#   def add_sub_task(task)
+#     @sub_tasks << task
+#   end
 
-  def remove_sub_task(task)
-    @sub_tasks.delete(task)
-  end
+#   def remove_sub_task(task)
+#     @sub_tasks.delete(task)
+#   end
 
-  def get_time_required
-    time = 0.0
-    @sub_tasks.each{ |task| time += task.get_time_required }
-    time
-  end
-end
+#   def get_time_required
+#     time = 0.0
+#     @sub_tasks.each{ |task| time += task.get_time_required }
+#     time
+#   end
+# end
 
-task = MakeBatterTask.new
+# task = MakeBatterTask.new
 
 # タスクの管理用のクラスを作る
 class CompositeTask < Task
@@ -99,7 +99,7 @@ class CompositeTask < Task
 end
 
 
-class MakeBatterTask < Task
+class MakeBatterTask < CompositeTask
 
   def initialize
     super('Make batter')
@@ -126,3 +126,141 @@ b.add_sub_task(MakeBatterTask.new)
 b.add_sub_task(MakeBatterTask.new)
 a.add_sub_task(b)
 p a.get_time_required
+
+
+class CompositeTask < Task
+
+  def initialize(name)
+    super(name)
+    @sub_tasks = []
+  end
+
+  def <<(task)
+    @sub_tasks << task
+  end
+
+  def [](index)
+    @sub_tasks[index]
+  end
+
+  def []=(index, new_value)
+    @sub_tasks[index] = new_value
+  end
+
+  def remove_sub_task(task)
+    @sub_tasks.delete(task)
+  end
+
+  def get_time_required
+    time = 0.0
+    @sub_tasks.each { |task| time += task.get_time_required }
+    time
+  end
+end
+
+a = MakeBatterTask.new
+a.<<(MakeBatterTask.new)
+a.<<(MakeBatterTask.new)
+a.<<(MakeBatterTask.new)
+a.<<(MakeBatterTask.new)
+a.<<(MakeBatterTask.new)
+a.<<(MakeBatterTask.new)
+b = MakeBatterTask.new
+b.<<(MakeBatterTask.new)
+b.<<(MakeBatterTask.new)
+b.<<(MakeBatterTask.new)
+b.<<(MakeBatterTask.new)
+b.<<(MakeBatterTask.new)
+b.<<(MakeBatterTask.new)
+a.<<(b)
+p a[0].get_time_required
+ a[0] = b
+p a[0].get_time_required
+p a.get_time_required
+
+# 親の参照をもつ
+class Task
+  attr_accessor :name, :parent
+
+  # タスク名
+  def initialize(name)
+    @name = name
+    @parent = nil
+  end
+
+  # 各タスクの所要時間
+  def get_time_required
+    0.0
+  end
+
+  def total_number_basic_tasks
+    1
+  end
+end
+
+class CompositeTask < Task
+
+  def initialize(name)
+    super(name)
+    @sub_tasks = []
+  end
+
+  def <<(task)
+    @sub_tasks << task
+    task.parent = self
+  end
+
+  def [](index)
+    @sub_tasks[index]
+  end
+
+  def []=(index, new_value)
+    @sub_tasks[index] = new_value
+  end
+
+  def remove_sub_task(task)
+    @sub_tasks.delete(task)
+    task.parent = nil
+  end
+
+  def get_time_required
+    time = 0.0
+    @sub_tasks.each { |task| time += task.get_time_required }
+    time
+  end
+
+  def total_number_basic_tasks
+    total = 0
+    @sub_tasks.each{|task| total += task.total_number_basic_tasks }
+    total
+  end
+end
+
+a = MakeBatterTask.new
+a.<<(MakeBatterTask.new)
+a.<<(MakeBatterTask.new)
+a.<<(MakeBatterTask.new)
+a.<<(MakeBatterTask.new)
+a.<<(MakeBatterTask.new)
+a.<<(MakeBatterTask.new)
+b = MakeBatterTask.new
+b.<<(MakeBatterTask.new)
+b.<<(MakeBatterTask.new)
+b.<<(MakeBatterTask.new)
+b.<<(MakeBatterTask.new)
+b.<<(MakeBatterTask.new)
+b.<<( c ||= MakeBatterTask.new)
+a.<<(b)
+p a[0].get_time_required
+ a[0] = b
+p a[0].get_time_required
+p a.get_time_required
+
+task = c.parent
+while true
+  p task.name
+  task = task.parent
+  break if task.nil?
+end
+
+p a.total_number_basic_tasks
